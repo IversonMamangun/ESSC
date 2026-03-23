cc03f15e20cfebe52366843e812b392964f02c67
IversonMamangun <177076443+IversonMamangun@users.noreply.github.com>
2026-03-23 10:50:13 +0800

    feat: add About route in SPA

--- resources/js/pages/Welcome.vue
+++ resources/js/pages/Welcome.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import About from '@/components/sections/About.vue';
 import Home from '@/components/sections/Home.vue';
 import Navbar from '@/components/sections/Navbar.vue';
 import TopBar from '@/components/sections/TopBar.vue';
@@ -16,6 +17,7 @@ withDefaults(
 
 <template>
     <TopBar />
-   <Navbar />
-   <Home />
+    <Navbar />
+    <Home />
+    <About />
 </template>
