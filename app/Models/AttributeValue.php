d090d82d6da4f10dfe487957420827dabe69d001
joshi <jespadilla422@gmail.com>
2026-05-18 16:08:49 +0800

    feat(model): add attribute related model with inverse relationship

---
+++ app/Models/AttributeValue.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class AttributeValue extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'attribute_id',
+        'value',
+    ];
+
+    public function attribute(): BelongsTo
+    {
+        return $this->belongsTo(Attribute::class);
+    }
+
+    public function productVariants(): BelongsToMany
+    {
+        return $this->belongsToMany(
+            ProductVariant::class,
+            'product_variant_attribute_values'
+        )
+        ->using(ProductVariantAttributeValue::class)
+        ->withTimestamps();
+    }
+}
