2b34ad10f460b217f8122744a316675f9372f0a2
joshi <jespadilla422@gmail.com>
2026-05-19 16:45:46 +0800

    feat(seller): add store and products props with resource collection

--- app/Http/Controllers/Seller/DashboardController.php
+++ app/Http/Controllers/Seller/DashboardController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Seller;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Product;
+use App\Http\Resources\Seller\ProductResource;
 use Inertia\Inertia;
 
 class DashboardController extends Controller
@@ -16,8 +18,21 @@ class DashboardController extends Controller
             return redirect()->route('seller.store.create');
         }
 
+        $products = Product::query()
+            ->where('store_id', $user->store->id)
+            ->with([
+                'categories',
+                'images',
+                'variants.attributeValues.attribute',
+            ])
+            ->latest()
+            ->paginate(10)
+            ->withQueryString();
+
         return Inertia::render('seller/dashboard/Index', [
-            
+            'store' => $user->store,
+            'products' => ProductResource::collection($products),
         ]);
+        
     }
 }
