44a3d6202c1ab042eef2ad5b203271afad154ed0
joshi <jespadilla422@gmail.com>
2026-05-20 19:27:47 +0800

    feat: configure attributes seeder and its value

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UserTypeSeeder::class,
             CategorySeeder::class,
+            AttributeSeeder::class
         ]);
 
         User::factory()->create([
