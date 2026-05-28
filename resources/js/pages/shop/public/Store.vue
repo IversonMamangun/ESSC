b630a7d853d687a2b3d67ec0d0b365e2fec7a294
IversonMamangun <177076443+IversonMamangun@users.noreply.github.com>
2026-05-28 17:56:24 +0800

    fix(Store.vue): remove duplicate import of Pagination component chore(eslint.config.js): disable unused vars rule for TypeScript and enable for Vue

--- resources/js/pages/shop/public/Store.vue
+++ resources/js/pages/shop/public/Store.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
 import { Head, router, usePage, Link } from '@inertiajs/vue3';
 import { ref, onMounted, nextTick, computed } from 'vue';
+import Pagination from '@/components/Pagination.vue';
 import ProductCard from '@/components/ProductCard.vue';
 import Footer from '@/components/sections/Footer.vue';
 import Navbar from '@/components/sections/Navbar.vue';
 import TopBar from '@/components/sections/TopBar.vue';
-import Pagination from '@/components/Pagination.vue';
 </script>
 
 <template>
