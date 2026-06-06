1d496f79bb2f924e1f96698064f160d2a0ac5d09
joshi <jespadilla422@gmail.com>
2026-06-06 17:01:29 +0800

    feat(migration): add user address, order, and payment related tables

---
+++ database/migrations/2026_06_06_073917_create_payments_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('order_id')->unique()->constrained()->cascadeOnDelete();
+            $table->foreignId('payment_method_id')->constrained()->restrictOnDelete();
+            $table->boolean('is_paid')->default(false);
+            $table->date('payment_date')->nullable();
+            $table->unsignedBigInteger('amount');
+            
+            $table->string('gateway')->nullable();
+            $table->string('gateway_payment_intent_id')->nullable()->index();
+            $table->string('gateway_payment_id')->nullable();
+            $table->json('gateway_response')->nullable();
+            $table->json('meta')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('payments');
+    }
+};
