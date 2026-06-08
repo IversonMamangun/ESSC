4efc626f8294f8b0699390baca8b278192bc40ca
joshi <jespadilla422@gmail.com>
2026-06-08 14:59:38 +0800

    feat: add psgc address composable and user address type

--- resources/js/types/auth.ts
+++ resources/js/types/auth.ts
@@ -25,3 +25,19 @@ export type TwoFactorConfigContent = {
   description: string;
   buttonText: string;
 };
+
+export type UserAddress = {
+  id: number;
+  label: string;
+  recipient_name: string;
+  recipient_number: string;
+  region: string;
+  province?: string;
+  city: string;
+  barangay: string;
+  street: string;
+  unit_bldg_house: string;
+  postal_code: string;
+  landmark?: string;
+  is_default: boolean;
+};
