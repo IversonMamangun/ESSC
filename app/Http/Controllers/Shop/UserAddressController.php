667059acc1587d81392a1646a899951d057d16f7
joshi <jespadilla422@gmail.com>
2026-06-09 14:57:17 +0800

    feat(shop): add creartion logic for user address with resource

--- app/Http/Controllers/Shop/UserAddressController.php
+++ app/Http/Controllers/Shop/UserAddressController.php
@@ -4,8 +4,12 @@ namespace App\Http\Controllers\Shop;
 
 use App\Http\Controllers\Controller;
 use App\Http\Resources\Shop\UserAddressResource;
+use App\Http\Requests\Shop\UserAddressCreateRequest;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
+use Illuminate\Support\Facades\DB;
+use App\Enums\UserAddressLabel;
+use App\Models\UserAddress;
 
 class UserAddressController extends Controller
 {
@@ -25,5 +29,35 @@ class UserAddressController extends Controller
         return Inertia::render('shop/customer/account/address/Create', [
             'user' => $user->only('name', 'phone', 'avatar'),
         ]);
+    }
+
+    public function store(UserAddressCreateRequest $request)
+    {
+        $validated = $request->validated();
+        $user = $request->user();
+
+        DB::transaction(function () use ($user, $validated) {
+
+            $isFirstAddress = !$user->addresses()->exists();
+
+            $user->addresses()->create([
+                ...$validated,
+                'is_default' => $isFirstAddress ? true : false
+            ]);
+        });
+
+        return redirect()->route('shop.account.addresses.index')
+            ->with('success', 'New address added successfully.');
+    }
+
+    public function destroy(Request $request, UserAddress $address)
+    {
+        if ($address->user_id !== $request->user()->id) {
+            abort(403);
+        }
+
+        $address->delete();
+
+        return back()->with('success', 'Address removed successfully.');
     }
 }
