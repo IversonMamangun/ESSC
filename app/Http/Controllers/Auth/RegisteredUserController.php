0a5b9fa7d9cf7c547a6b405c8c039b1d33a030ad
joshi <jespadilla422@gmail.com>
2026-06-20 17:06:46 +0800

    feat(auth): add related controller logic for otp registration and verification logic

---
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Http\Requests\Auth\CompleteRegistrationRequest;
+use Illuminate\Support\Facades\Auth;
+use App\Actions\Auth\CreateVerifiedUser;
+
+class RegisteredUserController extends Controller
+{
+    public function store(CompleteRegistrationRequest $request, CreateVerifiedUser $action) 
+    {
+        $user = $action->create(
+            $request->validated()
+        );
+
+        Auth::login($user);
+
+        return response()->json([
+            'message' => 'Registration successful.',
+            'redirect' => route('shop.home'),
+        ]);
+    }
+}
---
+++ app/Http/Controllers/Auth/VerificationController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\SendOtpRequest;
+use App\Http\Requests\Auth\VerifyOtpRequest;
+use App\Services\VerificationService;
+use Illuminate\Http\Request;
+
+class VerificationController extends Controller
+{
+    public function send(SendOtpRequest $request, VerificationService $service) 
+    {
+        $service->sendPhoneOtp(
+            $request->phone,
+            $request->purpose
+        );
+
+        return response()->json([
+            'message' => 'OTP sent.',
+        ]);
+    }
+
+    public function verify(VerifyOtpRequest $request, VerificationService $service) 
+    {
+        $token = $service->verifyPhoneOtp(
+            $request->phone,
+            $request->otp,
+            $request->purpose
+        );
+
+        return response()->json([
+            'verification_token' => $token,
+        ]);
+    }
+}
