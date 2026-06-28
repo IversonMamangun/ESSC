a874f42c5dbd2283653bd9b4f69e8f8e55729fad
joshi <jespadilla422@gmail.com>
2026-06-28 21:25:50 +0800

    feat(seller): add sales controller with index and action method logic with form request class validation

---
+++ app/Http/Requests/Seller/SalesActionRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Requests\Seller;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class SalesActionRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'action' => [
+                'required',
+                'string',
+                Rule::in([
+                    'deliver',
+                    'accept_return',
+                    'decline_return',
+                ]),
+            ],
+        ];
+    }
+}
